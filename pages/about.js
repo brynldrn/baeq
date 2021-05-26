38936c08ec01818751465f636a4803387e74e6dd
Bryan Aldrin Quinalayo <bryan.aldrin.quinalayo@codeandtheory.com>
2021-05-27 01:18:53 +0800

    ✨ Initial setup

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Navigation from './components/navigation'
 
-export default function Home() {
+export default function About() {
   return (
     <>
       <Head>
-        <title>Bryan Aldrin E Quinalayo &ndash; Full Stack Web Developer</title>
+        <title>About | Bryan Aldrin E Quinalayo &ndash; Full Stack Web Developer</title>
         <meta name="description" content="Professional Portfolio" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
         <link rel="icon" href="/favicon.ico" />
---
+++ pages/components/navigation.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Link from 'next/link';
+
+export default function Navigation() {
+  const [navActive, setNavActive] = useState(false);
+
+  const handleToggleClick = () => {
+    setNavActive(!navActive);
+  }
+
+  return (
+    <header className="navigation">
+      <div className="navigation__logo">
+        <Link href="/">
+          <a>.baeq()</a>
+        </Link>
+      </div>
+      <div className="navigation__links">
+        <button onClick={handleToggleClick} className={`navigation__trigger ${navActive ? '--active' : ''}`}>
+          <span className="material-icons material-icons-outlined">more_vert</span>
+          <span className="material-icons material-icons-outlined">close</span>
+        </button>
+        <nav className={`navigation__drawer ${ navActive ? '--active' : '' }`}>
+          <ul>
+            <li>
+              <Link href="/about">
+                <a>.about()</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/uses">
+                <a>.uses()</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
