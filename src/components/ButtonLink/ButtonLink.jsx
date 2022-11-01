5b6e132dbcfc55fa2fb7214803f67b78c117bf58
Bryan Aldrin <bryandummy01@gmail.com>
2022-11-01 17:17:15 +0800

    update lint errors

--- src/components/ButtonLink/ButtonLink.jsx
+++ src/components/ButtonLink/ButtonLink.jsx
@@ -12,8 +12,8 @@ export default function ButtonLink({ url = '', label = 'Open Link', buttonFace =
 
   return (
     <>
-      <Modal 
-        isOpen={modalOpen} 
+      <Modal
+        isOpen={modalOpen}
         setIsOpen={setModalOpen}
         link={url}
       >
@@ -23,16 +23,16 @@ export default function ButtonLink({ url = '', label = 'Open Link', buttonFace =
         internal ? (
           <Link href={url} >
             <a className={classNames('inline-flex items-center', className)}>
-              <span className='text-black bg-white rounded-full font-semibold w-6 inline-block text-center mr-3'>{buttonFace}</span> <span>{label}</span>
+              <span className="text-black bg-white rounded-full font-semibold w-6 inline-block text-center mr-3">{buttonFace}</span> <span>{label}</span>
             </a>
           </Link>
         ) : (
           <button onClick={onExternalClick} className={classNames('inline-flex items-center', className)}>
-            <span className='text-black bg-white rounded-full font-semibold w-6 inline-block text-center mr-3'>{buttonFace}</span> <span>{label}</span>
+            <span className="text-black bg-white rounded-full font-semibold w-6 inline-block text-center mr-3">{buttonFace}</span> <span>{label}</span>
           </button>
         )
       }
     </>
-    
+
   )
 }
--- src/components/NotificationBar/NotificationBar.jsx
+++ src/components/NotificationBar/NotificationBar.jsx
@@ -4,10 +4,10 @@ import Time from '../Time/Time';
 export default function NotificationBar() {
 
   return (
-    <div className='w-full flex justify-end absolute items-center top-0 left-0 text-white mt-4 z-[9]'>
+    <div className="w-full flex justify-end absolute items-center top-0 left-0 text-white mt-4 z-[9]">
       <Time className="mr-3" />
-      <Wifi className='mr-3' />
-      <BatteryCharging className='mr-3' />
+      <Wifi className="mr-3" />
+      <BatteryCharging className="mr-3" />
     </div>
   )
 }
